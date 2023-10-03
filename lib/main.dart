4a2c7d104689b9de858173b440c3b508e30efdf8
Sheng Qin <qinsheng32@gmail.com>
2023-10-03 02:53:44 -0700

    add SplashScreen screen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_gallery/routes.dart';
 
@@ -8,13 +9,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
+    return MaterialApp(
+      title: 'LENZ',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      routerConfig: router,
+      home: EasySplashScreen(
+        logo: Image.network(
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/BCIT_logo.svg/1129px-BCIT_logo.svg.png',
+          height: 200,
+          fit: BoxFit.fitHeight,
+        ),
+        title: const Text(
+          'LENZ',
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff084470),
+          ),
+        ),
+        backgroundColor: const Color(0xffffffff),
+        showLoader: true,
+        loaderColor: const Color(0xff084470),
+        loadingText: const Text(
+          'your story through… the eyes of a lens',
+          style: TextStyle(
+            fontSize: 12,
+            color: Color(0xff084470),
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        navigator: MaterialApp.router(
+          title: 'LENZ',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          routerConfig: router,
+        ),
+        durationInSeconds: 3,
+      ),
     );
   }
 }
