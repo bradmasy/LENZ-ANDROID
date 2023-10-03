277e300cf8ab5764078d4e0a2c429191bbbadabb
Sheng Qin <qinsheng32@gmail.com>
2023-10-03 03:47:36 -0700

    modify the Login page

--- lib/auth/view/login.dart
+++ lib/auth/view/login.dart
@@ -4,25 +4,123 @@ import 'package:go_router/go_router.dart';
 
 import '../auth_routes.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() { // Avoid using private types in public APIs.
+    return _LoginState();
+  }}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            backgroundColor: Colors.transparent,
             title: const Text('Login')),
         body: Center(
             child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Login'),
-            ElevatedButton(
-                onPressed: () => context.push(AuthRoutes.signup.path),
-                child: const Text('signup'))
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              'LENZ',
+              style: TextStyle(
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff084470),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+              child: TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Expanded(child: Container()),
+            SizedBox(
+              width: 120,
+              child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.0),
+                    ),
+                    backgroundColor: Colors.white,
+                    side: const BorderSide(
+                        color: Color(0xff084470),
+                        width: 4
+                    ),
+                  ),
+                  onPressed: () {
+                     _login();
+                  },
+                  child: const Text('login')),
+            ),
+            SizedBox(
+              width: 250,
+              child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.0),
+                    ),
+                    backgroundColor: Colors.white,
+                    side: const BorderSide(
+                        color: Colors.white,
+                        width: 0
+                    ),
+                  ),
+                  onPressed: () => context.push(AuthRoutes.signup.path),
+                  child: const Text('Don\'t have an account? Signup',
+                    style: TextStyle(
+                    ),
+                  )
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
           ],
         )));
+  }
+
+  Future<void> _login() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    print('login');
   }
 }
